937aa3c40e6440cf562917e61a8735da90adce87
yjymh <814123650@qq.com>
2021-04-20 12:28:39 +0800

    mybatis注解改为xml

--- Bot-dao/src/main/java/com/yjymh/robot/mapper/FriendTokenMapper.java
+++ Bot-dao/src/main/java/com/yjymh/robot/mapper/FriendTokenMapper.java
@@ -1,24 +1,19 @@
 package com.yjymh.robot.mapper;
 
 import com.yjymh.robot.entity.FriendToken;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FriendTokenMapper {
 
-    @Select("select * from friend where account = ${account}")
-    FriendToken queryFriendById(long account);
+    FriendToken queryFriendById(Long account);
 
-    @Select("select * from friend where token = '${token}'")
     FriendToken queryFriendByToken(String token);
 
-    @Insert("INSERT INTO `friend` (`account`, `token`, `status`, `update_time`, `create_time`) VALUES (#{account}, #{token}, #{status}, #{update_time}, #{create_time})")
     int addFriend(FriendToken friend);
 
-    @Update("UPDATE `friend` SET `token` = '${token}', `update_time` = '${update_time}' WHERE account = ${account}")
     int updateFriend(FriendToken friend);
 
-    @Delete("DELETE FROM `friend` WHERE `account`=${account}")
     int deleteFriend(Long account);
 
 }
--- Bot-dao/src/main/java/com/yjymh/robot/mapper/GroupTokenMapper.java
+++ Bot-dao/src/main/java/com/yjymh/robot/mapper/GroupTokenMapper.java
@@ -1,22 +1,19 @@
 package com.yjymh.robot.mapper;
 
 import com.yjymh.robot.entity.GroupToken;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface GroupTokenMapper {
-    @Select("select * from `group` where account = ${account}")
+
     GroupToken queryGroupById(long account);
 
-    @Select("select * from `group` where token = '${token}'")
     GroupToken queryGroupByToken(String token);
 
-    @Insert("INSERT INTO `group` (`account`, `token`, `status`, `update_time`, `create_time`) VALUES (#{account}, #{token}, #{status}, #{update_time}, #{create_time})")
     int addGroup(GroupToken group);
 
-    @Update("UPDATE `group` SET `token` = '${token}', `update_time` = '${update_time}' WHERE account = ${account}")
     int updateGroup(GroupToken group);
 
-    @Delete("DELETE FROM `group` WHERE account = ${group}")
     int deleteGroup(Long account);
+
 }
