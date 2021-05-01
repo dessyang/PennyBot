989323c2e1ef9e1994d52fe3a9b4c69c0534564c
yjymh <814123650@qq.com>
2021-05-02 00:44:58 +0800

    优化webhook发送消息的方法

--- Bot-web/src/main/java/com/yjymh/robot/controller/SendMessage.java
+++ Bot-web/src/main/java/com/yjymh/robot/controller/SendMessage.java
@@ -1,73 +1,40 @@
 package com.yjymh.robot.controller;
 
-import com.yjymh.robot.PennyBot;
-import com.yjymh.robot.entity.FriendToken;
-import com.yjymh.robot.entity.GroupToken;
 import com.yjymh.robot.entity.Response;
-import com.yjymh.robot.service.FriendTokenService;
-import com.yjymh.robot.service.GroupTokenService;
+import com.yjymh.robot.entity.Token;
+import com.yjymh.robot.service.TokenService;
 import com.yjymh.robot.utils.ResponseUtil;
-import net.mamoe.mirai.Bot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SendMessage {
 
     @Autowired
-    private FriendTokenService friendTokenService;
-
-    @Autowired
-    private GroupTokenService groupTokenService;
+    private TokenService tokenService;
 
     @PostMapping("/send")
-    public String send(@RequestParam String type,
-                       @RequestParam String token,
-                       @RequestParam String msg) {
-        Bot bot = PennyBot.getBot();
-        String response = ResponseUtil.setResponse(Response.PARAM_FAIL);
+    public String sendMessage(String type,
+                              String token,
+                              String msg) {
 
-        if (type.contains("group")) {
-            try {
-                if (msg != null && token != null) {
-                    GroupToken groupToken = groupTokenService.queryGroupByToken(token);
-                    if (groupToken != null) {
-                        Long group = groupToken.getAccount();
-                        if (group != null) {
-                            bot.getGroup(group).sendMessage(msg);
-                            return ResponseUtil.setResponse(Response.SUCCESS);
-                        }
-                    } else {
-                        response = ResponseUtil.setResponse(Response.TOKEN_FAIL);
-                    }
-                } else {
-                    response = ResponseUtil.setResponse(Response.PARAM_FAIL);
+        String response = null;
+
+        if (!type.equals("") && !token.equals("") && !msg.equals("")) {
+
+            Token tokenInfo = tokenService.getTokenInfo(type, token);
+
+            if (tokenInfo != null) {
+                Long account = tokenInfo.getAccount();
+                if (account != null) {
+                    response = tokenService.sendMessageByAccount(type, account, msg);
                 }
-            } catch (Exception e) {
-                response = ResponseUtil.setResponse(Response.FAIL);
+            } else {
+                response = ResponseUtil.setResponse(Response.TOKEN_FAIL);
             }
-        } else if (type.contains("friend")) {
-            try {
-                if (msg != null && token != null) {
-                    FriendToken friendToken = friendTokenService.queryFriendByToken(token);
-                    if (friendToken != null) {
-                        Long id = friendToken.getAccount();
-                        if (id != null) {
-                            bot.getFriend(id).sendMessage(msg);
-                            response = ResponseUtil.setResponse(Response.SUCCESS);
-                        }
-                    } else {
-                        response = ResponseUtil.setResponse(Response.TOKEN_FAIL);
-                    }
-                } else {
-                    response = ResponseUtil.setResponse(Response.PARAM_FAIL);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                response = ResponseUtil.setResponse(Response.FAIL);
-            }
+        } else {
+            response = ResponseUtil.setResponse(Response.PARAM_FAIL);
         }
         return response;
     }
